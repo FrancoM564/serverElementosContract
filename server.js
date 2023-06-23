fda44c5fcc5d073b5967996c4654b9ab6c20724e
FrancoM564 <franco1.alfra@gmail.com>
2023-06-23 03:43:54 -0500

    añadido forma de guardar csv

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const app = express();
 const port = 3000;
 
 app.use(cors());
+app.use(express.json());
+
 
 // Ruta para obtener la metadata, cambiar los valores en base al pc
 app.get('/metadata.json', (req, res) => {
@@ -30,3 +32,34 @@ app.get('/codeReport.wasm', (req, res) => {
   res.sendFile('/home/franco/Documentos/develop/sem1/dsApp/serverElementosContract/contracts/report/contract_report.wasm');
 });
 
+// Ruta para recibir los valores y almacenarlos en el archivo CSV
+app.post('/guardar', (req, res) => {
+  // Obtener los valores enviados en la solicitud
+
+  console.log(req.body)
+
+  const { numeroIteracion, tiempoEjecucion } = req.body;
+
+  // Validar los valores o realizar cualquier otra lógica necesaria
+
+  // Crear una instancia del escritor CSV
+  const createCsvWriter = require('csv-writer').createObjectCsvWriter;
+  const csvWriter = createCsvWriter({
+    path: 'datos.csv', // Nombre del archivo CSV donde se almacenarán los datos
+    header: [
+      { id: 'numeroIteracion', title: 'Numero de iteracion' },
+      { id: 'tiempoEjecucion', title: 'Tiempo de ejecucion' }
+    ],
+    append: true
+  });
+
+  // Escribir los valores en el archivo CSV
+  csvWriter.writeRecords([{ numeroIteracion, tiempoEjecucion }])
+    .then(() => {
+      res.send('Valores almacenados correctamente en el archivo CSV.');
+    })
+    .catch((error) => {
+      console.error('Error al escribir en el archivo CSV:', error);
+      res.status(500).send('Ocurrió un error al almacenar los valores.');
+    });
+});
